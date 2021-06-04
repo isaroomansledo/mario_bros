9b43d93e4cfc0d473a91f940f9283e3ad173e739
isaroomansledo <ir319@ic.ac.uk>
2021-06-04 14:26:53 +0200

    fixed coin counter
    The counter was not working properly

--- 2DPlatformer/Assets/Scripts/Counter.cs
+++ 2DPlatformer/Assets/Scripts/Counter.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-    public void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag=="Player")
         {
-            ScoreTextScript.coinAmount += 1;
-            gameObject.SetActive(false);
-           
+          ScoreTextScript.coinAmount += 1;
+          Destroy (gameObject);
         }
-
+        
     }
 }
