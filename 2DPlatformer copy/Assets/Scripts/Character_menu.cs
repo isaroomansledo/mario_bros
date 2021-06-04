ee8114498366ffca6d3ee76bfe8b4415e4b35382
MichaelaJurkovicova <mj719@ic.ac.uk>
2021-06-04 18:46:51 +0100

    characters?

---
+++ 2DPlatformer copy/Assets/Scripts/Character_menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character_menu : MonoBehaviour
+{
+    Sprite[] spritesheet;
+    int colourID;
+    Image representation;
+
+    //Use this for initialization
+    void Start()
+    {
+        representation = GameObject.Find("Representation").GetComponent<Image>();
+        colourID = 0;
+     
+        spritesheet = Resources.LoadAll<Sprite>("Sprites/RobotBoyIdleSprite");
+    }
+
+    public void ChangeColour()
+    {
+        if (colourID < 5)
+        {
+            colourID++;
+            Debug.Log(colourID);
+        }
+        else if (colourID == 5)
+        {
+            colourID = 0;
+            Debug.Log(colourID);
+        }
+
+        foreach (Sprite S in spritesheet)
+        {
+            if (S.name.Equals("RobotBoyIdle" + "0" + colourID))
+            {
+                representation.sprite = S;
+                Global_variables.material = "0" + colourID;
+                Debug.Log(Global_variables.material);
+            }
+        }
+    }
+}
+
+    
---
+++ 2DPlatformer copy/Assets/Scripts/Counter.cs
@@ -0,0 +1,17 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+          ScoreTextScript.coinAmount += 1;
+          Destroy (gameObject);
+        }
+        
+    }
+}
