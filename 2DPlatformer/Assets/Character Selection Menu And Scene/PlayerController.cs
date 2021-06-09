da3b8ccb37c53d356c2c37b0e45b165ded341f37
DW2001 <76006428+DW2001@users.noreply.github.com>
2021-06-10 00:10:53 +0100

    character menu (read descrition)
    New character menu
    Maybe fixed aspect ratio for the game window and thus ui elements? Test it and tell me what you see.
    Hopefully didn't break the whole thing :)

---
+++ 2DPlatformer/Assets/Character Selection Menu And Scene/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool enabled;
+    public float playerSpeed;
+    Animator animator;
+    SpriteRenderer sprite;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (enabled)
+        {
+            float input = Input.GetAxis("Horizontal");
+            if (input > 0.5f)
+            {
+                animator.SetBool("Walk", true);
+                sprite.flipX = false;
+
+            }
+            else if (input < -0.5f)
+            {
+                animator.SetBool("Walk", true);
+                sprite.flipX = true;
+
+            }
+            else
+            {
+                animator.SetBool("Walk", false);
+            }
+            transform.position += input * Vector3.right * playerSpeed * Time.deltaTime;
+        }
+
+    }
+}
---
+++ 2DPlatformer/Assets/Scripts/AutoAddPlayerToVcamTargets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class AutoAddPlayerToVcamTargets : MonoBehaviour
+{
+    [TagField]
+    public string Tag = string.Empty;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        var vcam = GetComponent<CinemachineVirtualCameraBase>();
+        if (vcam != null && Tag.Length > 0)
+        {
+            var targets = GameObject.FindGameObjectsWithTag(Tag);
+            if (targets.Length > 0)
+                vcam.LookAt = vcam.Follow = targets[0].transform;
+        }
+    }
+}
